2dfeea2317ac8832483100d582dbb4a425cee937
root <khozameron5@gmail.com>
2023-10-31 06:46:11 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -34,11 +34,11 @@ while (total > 1)
 {
 for (i = 0; i < sizeof(cents[i]); i++)
 {
-if (total >= cents[i])
-{
-count += total / cents[i];
-total = total % cents[i];
-}
+   if (total >= cents[i])
+   {
+       count += total / cents[i];
+       total = total % cents[i];
+   }
 }
 }
 if (total == 1)
@@ -48,6 +48,9 @@ else
 {
 printf("Error\n");
 return (1);
+}
+
+
 printf("%d\n", count);
 return (0);
 }
