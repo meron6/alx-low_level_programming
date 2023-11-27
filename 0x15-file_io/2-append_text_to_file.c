091f9f97aff081134d1450bacccc6ad965e3eb01
meron6 <khozameron5@gmail.com>
2023-11-27 14:42:53 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,23 +3,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/**
- * create_file - creates a file with the specified permissions and writes the
- * specified text to it
- * @filename: the name of the file to create
- * @text_content: a NULL-terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
- */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, bytes_written;
+nt fd, bytes_written;
 mode_t mode = S_IRUSR | S_IWUSR;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
