25ebbedd4a4ba7c6cc07f5639deaa5cc43e86c85
meron6 <khozameron5@gmail.com>
2023-12-01 11:54:50 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * create_file - creates a file with the specified permissions and writes the
@@ -13,22 +14,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, bytes_written;
-mode_t mode = S_IRUSR | S_IWUSR;
-if (filename == NULL)
-return (-1);
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (fd == -1)
-return (-1);
-if (text_content != NULL)
-{
-bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
-{
-close(fd);
-return (-1);
+    int fd, bytes_written;
+    mode_t mode = S_IRUSR | S_IWUSR;
+
+    if (filename == NULL)
+        return (-1);
+
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
 }
-}
-close(fd);
-return (1);
-}
+
