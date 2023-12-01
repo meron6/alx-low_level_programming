f3c3f93fe15aafd241364bb08b8998a6f165eecd
meron6 <khozameron5@gmail.com>
2023-12-01 11:49:55 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 
+/**
+ * read_textfile - This reads a text file and prints it to the POSIX stdo
+ *
+ * @filename: names of the file to read
+ *
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print 
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t bytes_read, bytes_written;
+int file_d;
+ssize_t lenr, lenw;
 char *buffer;
 
 if (filename == NULL)
 return (0);
-
+file_d = open(filename, O_RDONLY);
+if (file_d == -1)
+return (0);
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
+{
+close(file_d);
 return (0);
-
-fd = open(filename, O_RDONLY);
-if (fd == -1)
+}
+lenr = read(file_d, buffer, letters);
+close(file_d);
+if (lenr == -1)
 {
 free(buffer);
 return (0);
 }
-
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-{
+lenw = write(STDOUT_FILENO, buffer, lenr);
 free(buffer);
-close(fd);
+if (lenr != lenw)
 return (0);
-}
-
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-free(buffer);
-close(fd);
-return (0);
-}
-
-free(buffer);
-close(fd);
-return (bytes_written);
+return (lenw);
 }
