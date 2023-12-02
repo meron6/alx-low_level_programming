cb712a9fe3ac0750a48057eef1f3cc5ae2bba55b
meron6 <khozameron5@gmail.com>
2023-12-02 10:43:12 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - Read text file print to STDOUT.
@@ -10,19 +12,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *burf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
+ssize_t n_read, n_written;
+char *buffer;
+int file;
 
-fd = open(filename, O_RDONLY);
-if (fd == -1)
+if (filename == NULL)
 return (0);
-buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
 
-free(buf);
-close(fd);
-return (w);
+file = open(filename, O_RDONLY);
+if (file == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+close(file);
+return (0);
+}
+
+n_read = read(file, buffer, letters);
+n_written = write(STDOUT_FILENO, buffer, n_read);
+
+free(buffer);
+close(file);
+
+if (n_written != n_read)
+return (0);
+
+return (n_written);
 }
