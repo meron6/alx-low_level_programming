a2a67a2c07bac472aa57fa54e71d45a832c479ac
meron6 <khozameron5@gmail.com>
2023-12-02 11:04:42 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
- * create_file - creates a file with the specified permissions and writes the
- * specified text to it
- * @filename: the name of the file to create
- * @text_content: a NULL-terminated string to write to the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written;
-    mode_t mode = S_IRUSR | S_IWUSR;
+int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-
-    close(fd);
-    return (1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
 }
 
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+
+if (fd == -1 || w == -1)
+return (-1);
+
+close(fd);
+
+return (1);
+}
